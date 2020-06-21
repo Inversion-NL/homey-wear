8523fdbf39fe796012a21588935acbee0da132b3
RoyK <rkokk@protonmail.com>
2020-06-22 01:02:13 +0200

    Move ColorRunner to thread implemenation

--- app/src/main/java/com/xseth/homey/utils/ColorRunner.java
+++ app/src/main/java/com/xseth/homey/utils/ColorRunner.java
@@ -1,7 +1,6 @@
 package com.xseth.homey.utils;
 
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 
@@ -10,63 +9,45 @@ import java.util.Random;
 /**
  * Class for updating background color of views
  */
-public class ColorRunner extends AsyncTask<View, Integer, Void> {
+public class ColorRunner {
 
     // Logger TAG
     public static final String TAG = "ColorRunner";
     // Amount of time in MS to wait between color switches
-    public static final int SLEEP = 250;
+    public static final int SLEEP = 500;
     // Resolution of color possibilities
     public static final int COLOR_SIZE = 500;
     // Array depicting rainbow colors
     public static int[] COLORS = new int[COLOR_SIZE];
-    // View for which background is updated
-    private View view;
 
-    public static void startColorRunner(View v){
-        new ColorRunner(v).execute();
-    }
-
-    public ColorRunner(View v){
-        view = v;
-
-        // Generate colors
+    public static void startColorRunner(View view){
         double jump = 360.0 / (COLOR_SIZE*1.0);
         for (int i = 0; i < COLORS.length; i++) {
             COLORS[i] = Color.HSVToColor(new float[]{(float) (jump*i), 1.0f, 1.0f});
         }
-    }
 
-    protected void onProgressUpdate(Integer... colors) {
-        Log.v(TAG, "Setting color: "+colors[0]);
-        view.setBackgroundColor(colors[0]);
-    }
+        // Change color in background
+        new Thread(() -> {
+            int color;
+            boolean run = true;
+            int max = COLORS.length;
+            int index = new Random().nextInt(COLOR_SIZE-1);
 
-    @Override
-    protected Void doInBackground(View... views) {
-        int index = new Random().nextInt(COLOR_SIZE-1);
-        int color;
-        int max = COLORS.length;
+            Log.d(TAG, "Starting colorRun");
 
-        Log.d(TAG, "Starting run");
+            do {
+                color = COLORS[index];
+                view.setBackgroundColor(color);
 
-        do {
-            color = COLORS[index];
-            publishProgress(color);
+                index += 1;
 
-            index += 1;
+                if (index == max)
+                    index = 0;
 
-            if (index == max)
-                index = 0;
-
-            try {
-                Thread.sleep(SLEEP);
-            } catch (InterruptedException ignored) {}
-        } while (true);
-    }
-
-    @Override
-    protected void onPostExecute(Void result) {
-        Log.v(TAG, "onPostExecute");
+                try {
+                    Thread.sleep(SLEEP);
+                } catch (InterruptedException ignored) { run = false; }
+            } while (run);
+        }).start();
     }
 }
