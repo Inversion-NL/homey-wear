771ba119def53fb27812923bd59df1833e870c8b
RoyK <rkokk@protonmail.com>
2020-07-06 00:59:34 +0200

    Code cleanup in DeviceViewModel

--- app/src/main/java/com/xseth/homey/adapters/DeviceViewModel.java
+++ app/src/main/java/com/xseth/homey/adapters/DeviceViewModel.java
@@ -6,7 +6,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.xseth.homey.homey.models.Device;
-import com.xseth.homey.homey.models.DeviceRepository;
+import com.xseth.homey.homey.DeviceRepository;
 
 import java.util.List;
 
@@ -44,8 +44,7 @@ public class DeviceViewModel extends AndroidViewModel {
      */
     public void refreshDevices(){
         // Remove all devices from viewModel & DB
-        for(Device dev : this.devices.getValue())
-            mRepository.deleteDevices();
+        mRepository.deleteDevices();
 
         // Force reloading of all devices
         mRepository.getAllDevices(true);
