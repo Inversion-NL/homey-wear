15853648c730dd697f87d8573ffaebf9d20563e1
RoyK <rkokk@protonmail.com>
2020-07-24 12:31:28 +0200

    Update ColorRunner to start/stop/pause/unpause

--- app/src/main/java/com/xseth/homey/utils/ColorRunner.java
+++ app/src/main/java/com/xseth/homey/utils/ColorRunner.java
@@ -10,47 +10,91 @@ import timber.log.Timber;
 /**
  * Class for updating background color of views
  */
-public class ColorRunner {
+public class ColorRunner implements Runnable{
 
+    // Thread object
+    private static ColorRunner INSTANCE;
+    // Boolean used to start/stop runnable
+    private boolean run = true;
+    // Boolean used to pause/unpause runnable
+    private boolean pause = false;
     // Amount of time in MS to wait between color switches
-    public static final int SLEEP = 500;
+    public final int SLEEP = 500;
     // Resolution of color possibilities
-    public static final int COLOR_SIZE = 500;
+    public final int COLOR_SIZE = 500;
     // Array depicting rainbow colors
-    public static int[] COLORS = new int[COLOR_SIZE];
+    public int[] COLORS = new int[COLOR_SIZE];
+    // View object to colorize
+    private View view;
 
     /**
      * Start the ColorRunner, thread which changes background color
      * @param view View for which background color is updated
      */
     public static void startColorRunner(View view){
+        // Change color in background
+        INSTANCE = new ColorRunner(view);
+        new Thread(INSTANCE).start();
+    }
+
+    /**
+     * Resume paused ColorRunner Thread
+     */
+    public static void resumeColorRunner(){
+        INSTANCE.pause = false;
+    }
+
+    /**
+     * Pause running ColorRunner Thread
+     */
+    public static void pauseColorRunner(){
+        INSTANCE.pause = true;
+    }
+
+    /**
+     * Stop ColorRunner Thread
+     */
+    public static void stopColorRunner(){
+        INSTANCE.run = false;
+    }
+
+    private ColorRunner(View view){
+        this.view = view;
+
         double jump = 360.0 / (COLOR_SIZE*1.0);
         for (int i = 0; i < COLORS.length; i++) {
             COLORS[i] = Color.HSVToColor(new float[]{(float) (jump*i), 1.0f, 1.0f});
         }
+    }
 
-        // Change color in background
-        new Thread(() -> {
-            int color;
-            boolean run = true;
-            int max = COLORS.length;
-            int index = new Random().nextInt(COLOR_SIZE-1);
+    @Override
+    public void run() {
+        int color;
 
-            Timber.d("Starting colorRun");
+        int max = COLORS.length;
+        int index = new Random().nextInt(COLOR_SIZE-1);
 
-            do {
-                color = COLORS[index];
-                view.setBackgroundColor(color);
+        Timber.d("Starting colorRun");
 
-                index += 1;
-
-                if (index == max)
-                    index = 0;
-
+        do {
+            // pause remain in sleep forever until unpause
+            while(pause){
                 try {
-                    Thread.sleep(SLEEP);
-                } catch (InterruptedException ignored) { run = false; }
-            } while (run);
-        }).start();
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+            }
+
+            color = COLORS[index];
+            view.setBackgroundColor(color);
+
+            index += 1;
+
+            if (index == max)
+                index = 0;
+
+            try {
+                Thread.sleep(SLEEP);
+            } catch (InterruptedException ignored) { run = false; }
+        } while (run);
     }
 }
